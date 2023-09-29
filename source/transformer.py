a555cac5f8eb15142b9f4b8bbab7dd3d011e2b78
Dr. Tristan Behrens <tristan@ai-guru.de>
2023-09-29 08:37:43 +0200

    Removes nanoGPT import.

--- source/transformer.py
+++ source/transformer.py
@@ -6,8 +6,33 @@ import torch
 import torch.nn as nn
 from torch.nn import functional as F
 
-from nanoGPT.model import LayerNorm, MLP
 
+class LayerNorm(nn.Module):
+    """ LayerNorm but with an optional bias. PyTorch doesn't support simply bias=False """
+
+    def __init__(self, ndim, bias):
+        super().__init__()
+        self.weight = nn.Parameter(torch.ones(ndim))
+        self.bias = nn.Parameter(torch.zeros(ndim)) if bias else None
+
+    def forward(self, input):
+        return F.layer_norm(input, self.weight.shape, self.weight, self.bias, 1e-5)
+
+class MLP(nn.Module):
+
+    def __init__(self, config):
+        super().__init__()
+        self.c_fc    = nn.Linear(config.n_embd, 4 * config.n_embd, bias=config.bias)
+        self.gelu    = nn.GELU()
+        self.c_proj  = nn.Linear(4 * config.n_embd, config.n_embd, bias=config.bias)
+        self.dropout = nn.Dropout(config.dropout)
+
+    def forward(self, x):
+        x = self.c_fc(x)
+        x = self.gelu(x)
+        x = self.c_proj(x)
+        x = self.dropout(x)
+        return x
 
 
 class CausalSelfAttentionNoMask(nn.Module):
