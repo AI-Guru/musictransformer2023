Fixes bottleneck masking

source/dataset.py
@@ -197,7 +197,7 @@ class Dataset:
             encoder_ids = torch.stack(encoder_ids, dim=0).long()
             decoder_ids = torch.stack(decoder_ids, dim=0).long()
             target_ids = torch.stack(target_ids, dim=0).long()
-            padding_masks = torch.stack(padding_masks, dim=0).bool()
+            padding_masks = torch.stack(padding_masks, dim=0).float()
 
             # Return.
             return encoder_ids, decoder_ids, target_ids, padding_masks

tests/test_transformer.py
@@ -24,7 +24,7 @@ def test():
     # x is a sequence of integers.
     sequence_length = transformer_config.block_size
     vocab_size = transformer_config.vocab_size
-    batch_size = 1
+    batch_size = 128
     encoder_ids = torch.randint(0, vocab_size, (batch_size, sequence_length))
     print(f"encoder_ids.shape: {encoder_ids.shape}")
     print(f"encoder_ids: {encoder_ids}")